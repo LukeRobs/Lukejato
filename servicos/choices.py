52adb8130ac34891319f714b67edd08d060588b2
LukeRobs <lucassrobson06@gmail.com>
2023-08-19 12:00:53 -0300

    gerando pdf e terminando listagem de pedidos

--- servicos/choices.py
+++ servicos/choices.py
@@ -4,3 +4,10 @@ class ChoicesCategoriaManutencao(TextChoices):
     TROCAR_VALVULA_MOTOR =  "TVM", "Trocar válvula do motor",
     TROCAR_OLEO = "TO", "Troca de óleo"
     BALANCEMANTO = "B", "Balanceamento"
+    TROCAR_MOTOR = "TM", "Trocar Motor"
+    TROCAR_PNEU = "TP", "Trocar Pneu"
+    CAIXA_MARCHA = "CM", "Trocar caixa de marcha"
+    PINTURA = "P", "PINTAR O CARRO"
+    DESAMASSAR = "D", "Desamassar"
+    ALINHAMENTO = "A", "Alinhamento"
+    
